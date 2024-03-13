b639ef8ae52c90fc7cebfd729c693f3a3ce8d564
AMAN-B-H-A-T-T <22csama003@ldce.ac.in>
2024-03-13 19:55:31 +0530

    initial setup of server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors')
+require('dotenv').config()
+const { connect_db } = require('./utils/db_connect');
+// initilized the port, for handling all kinds of request on server
+const PORT = 4000;
+// initilized the express server 
+const app = express()
+
+
+
+
+
+try{
+    // include the body parser middleware
+    app.use(bodyParser.json())
+    //setup the cors 
+    app.use(cors())
+    connect_db()
+}
+catch(error){
+    console.log(error.message)
+}
+
+// listen the requests on the port 
+app.listen(PORT,()=>{
+    console.log("http://localhost:4000")
+})
